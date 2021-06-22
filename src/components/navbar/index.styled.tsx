00b1e6ef1a98f12b750cbe59ccfe61a453d25c98
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 01:55:02 +0545

    Created index.styled.css

---
+++ src/components/navbar/index.styled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-right: 1rem;
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const LogoText = styled.h1`
+  font-size: 1.2rem;
+  padding-left: 0.5rem;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
