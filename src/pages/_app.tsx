4f92c9303c2d515d11911a7a6f4d581e9c40f686
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-22 23:54:40 +0545

    Added layout support in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from "next/app";
 
+import BaseLayout from "../components/layout/BaseLayout";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const component: any = Component;
+  const Layout = component.Layout || BaseLayout;
+
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 export default MyApp;
