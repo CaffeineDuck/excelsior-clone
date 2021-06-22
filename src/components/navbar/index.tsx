87b81485d37550b2f557e9cc4012fdd5e45131fd
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 01:55:30 +0545

    Moved styles from index.css to index.styles.css for better maintainability

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import ExcelsiorLogo from "../../../public/assets/excelsior-logo.png";
+import { LogoContainer, LogoText, Wrapper } from "./index.styled";
+import Image from 'next/image'
+const NavBar: React.FC = () => {
+  return (
+    <>
+      <Wrapper>
+        <LogoContainer>
+          <Image height="60px" width="60px" src={ExcelsiorLogo} />
+          <LogoText>The Excelsior School</LogoText>
+        </LogoContainer>
+        <div>
+          Lol
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+
+export default NavBar;
