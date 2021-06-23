11395b30180dc75caa38f1409fe5dcb2900a4f56
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 07:29:29 +0545

    Added fonts

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,22 +1,29 @@
 import * as React from "react";
+import Image from "next/image";
+import  Head  from "next/head";
 
 import ExcelsiorLogo from "../../../public/assets/excelsior-logo.png";
 import { LogoContainer, LogoText, Wrapper } from "./index.style";
-import Image from 'next/image'
-const NavBar: React.FC = () => {
+
+const NavBar = () => {
   return (
     <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Wrapper>
         <LogoContainer>
-          <Image height="60px" width="60px" src={ExcelsiorLogo} />
+          <Image height="55px" width="58px" src={ExcelsiorLogo} />
           <LogoText>The Excelsior School</LogoText>
         </LogoContainer>
-        <div>
-          Lol
-        </div>
+        <div>Lol</div>
       </Wrapper>
     </>
   );
 };
+
 
 export default NavBar;
