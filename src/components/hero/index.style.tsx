93e9f2ec2a54868e07323524a4fda7c49bc8c441
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-24 00:40:51 +0545

    Added the animation effect

--- src/components/hero/index.style.tsx
+++ src/components/hero/index.style.tsx
@@ -1,15 +1,59 @@
 import styled from "styled-components";
-import Image from "next/image";
 
 export const MainWrapper = styled.div`
   display: grid;
   grid-template-rows: 20fr 10fr;
   height: 100vh;
   width: 100%;
-
 `;
 
-export const ImageSlides = styled(Image)`
-  z-index: 1;
+export const ImageWrapper = styled.div`
   object-fit: cover;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Figure = styled.figure<{
+  backgroundImage: string;
+  animationDelay: number;
+}>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  animation: slideShow 24s linear infinite 0s;
+
+  @keyframes slideShow {
+    0% {
+      opacity: 0;
+      transform: scale(1);
+    }
+
+    5% {
+      opacity: 1;
+    }
+
+    25% {
+      opacity: 1;
+    }
+
+    30% {
+      opacity: 0;
+      transform: scale(1.1);
+    }
+
+    100% {
+      opacity: 0;
+      transform: scale(1);
+    }
+  }
+`;
+
+export const SlideImage = styled(Figure)`
+  animation-delay: ${(props) => props.animationDelay}s;
+  background: url(${(props) => props.backgroundImage});
+  background-size: cover;
+  position: absolute;
 `;
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,16 +1,18 @@
-import About from "./About";
-import { ImageSlides, MainWrapper} from "./index.style";
-
-import Image1 from "../../../public/assets/hero/1.jpg";
 import React from "react";
+
+import About from "./About";
+import { SlideImage, ImageWrapper, MainWrapper } from "./index.style";
 
 const HeroSection: React.FC = () => {
   return (
     <>
       <MainWrapper>
-        <React.Fragment>
-          <ImageSlides src={Image1} />
-        </React.Fragment>
+        <ImageWrapper>
+          <SlideImage backgroundImage="assets/hero/0.jpg" animationDelay={0}/>
+          <SlideImage backgroundImage="assets/hero/1.jpg" animationDelay={6}/>
+          <SlideImage backgroundImage="assets/hero/2.jpg" animationDelay={12}/>
+          <SlideImage backgroundImage="assets/hero/3.jpg" animationDelay={18}/>
+        </ImageWrapper>
         <About />
       </MainWrapper>
     </>
