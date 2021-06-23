7f95186cca177d54efb5da6e6681d1548f719cfb
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 07:29:09 +0545

    Added global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
 import BaseLayout from "../components/layout/BaseLayout";
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const component: any = Component;
