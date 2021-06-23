a90f2efbfb6a2799923543389e8ac094e8140ce9
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 21:31:11 +0545

    Fixed About section

--- src/components/hero/About.style.tsx
+++ src/components/hero/About.style.tsx
@@ -1,16 +1,33 @@
 import styled from "styled-components";
 
+export const UpperWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export const Wrapper = styled.div`
-  padding-left: 5rem;
-  padding-right: 5rem;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
+  margin-top: 15vh;
+  margin-bottom: 2vh;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const InnerWrapper = styled.div`
+  max-width: 960px;
 `
 
-export const AboutHeader = styled.div`
-  font-size: 1.7em;
+export const AboutHeader = styled.h1`
+  font-size: 1.6rem;
   font-family: "Work Sans", sans-serif;
   color: #232323;
   font-weight: 600;
   text-transform: uppercase;
+`;
+
+export const AboutBody = styled.p`
+  padding-top: 1rem;
+  font-size: 1rem;
+  line-height: 1.5rem;
 `;
--- src/components/hero/About.tsx
+++ src/components/hero/About.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Head from "next/head";
-import { AboutHeader } from "./About.style";
+import { AboutBody, AboutHeader, InnerWrapper, Wrapper } from "./About.style";
 
 const About: React.FC = () => {
   return (
@@ -13,9 +13,26 @@ const About: React.FC = () => {
           rel="stylesheet"
         />
       </Head>
-      <AboutHeader>
-        About Us
-      </AboutHeader>
+      <Wrapper>
+        <InnerWrapper>
+          <AboutHeader>About Us</AboutHeader>
+          <AboutBody>
+            The Excelsior School is an award-winning school located in the heart
+            of the city with a pristine and child friendly environment. We
+            strive for quality education at affordable cost by using recent
+            pedagogies so as to attain academic excellence. Our prime focus is
+            on developing knowledgeable, inquiring and caring young people who
+            can help to create a better and more peaceful world. Thus, we strive
+            equipping our young mind with technical, social and academic skills
+            to stand in the global community making significant contributions in
+            creating habitable society. Our academic and extracurricular
+            activities help to enhance students’ attitude and skills to be
+            lifelong learners. In our happy and child friendly place of
+            excellence, everyone can recognize and achieve their full potential
+            in all areas of their development.
+          </AboutBody>
+        </InnerWrapper>
+      </Wrapper>
     </>
   );
 };
