5b26fc509b1f92d2a0552d0c4771fe34db321d09
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-23 07:29:51 +0545

    Changed the font

--- src/components/navbar/index.style.tsx
+++ src/components/navbar/index.style.tsx
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin-right: 1rem;
-  margin-left: 1rem;
+  padding-right: 2rem;
+  padding-left: 2rem;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  background-color: #232555;
 `;
 
 export const LogoText = styled.h1`
   font-size: 1.2rem;
   padding-left: 0.5rem;
+  color: #fff;
+  font-weight: 500;
+  font-family: 'Questrial', sans-serif;
 `;
 
 export const LogoContainer = styled.div`
   display: flex;
   flex-direction: row;
+  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
 `;
