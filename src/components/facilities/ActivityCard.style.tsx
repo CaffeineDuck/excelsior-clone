1c96aa95c89384c9e5822268c46267b37fa6b9ee
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-27 16:12:15 +0545

    Added the hover transition

--- src/components/facilities/ActivityCard.style.tsx
+++ src/components/facilities/ActivityCard.style.tsx
@@ -5,21 +5,49 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  width: 99vw;
   min-height: 50vh;
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const Card = styled.div<{ src: string }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transition: transform 0.8s ease;
 `;
 
 export const CardWrapper = styled.div`
   position: relative;
   cursor: pointer;
-`;
 
-export const Card = styled.img`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: 1;
-  object-fit: cover;
+  &:before {
+    content: "";
+    cursor: pointer;
+    background: 0 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 8;
+    transition: background 0.8s ease;
+  }
+  &:hover:before {
+    background: rgba(0, 0, 0, 0.6);
+  }
+
+  &:hover ${Card} {
+    content: "";
+    transform: scale(1.05);
+  }
 `;
 
 export const CardText = styled.h2`
